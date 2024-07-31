36064a7b12e17086d94c423c2e46d27525d1d9b9
RanRod <raihanandromedacentury99@gmail.com>
2024-08-01 06:02:34 +0700

    Monsoonsim-Retail-Tools

--- HomePage.py
+++ HomePage.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Page configuration
 st.set_page_config(
-    page_title="Monsoonsim - Retail Module Tools",
+    page_title="Monsoonsim - Retail Module Tools Calculation",
     page_icon="🏪",
     layout="wide",
     initial_sidebar_state="expanded",
